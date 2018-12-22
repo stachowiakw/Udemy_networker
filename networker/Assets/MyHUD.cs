0b1eb9a66f29fbe8fbd6ed517e9d73fc990eef5b
Wojciech Stachowiak <stachowiakw@gmail.com>
2018-12-23 00:02:05 +0100

    2nd commit

---
+++ networker/Assets/MyHUD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyHUD : MonoBehaviour {
+    private NetworkManager networkManager;
+
+	// Use this for initialization
+	void Start () {
+        networkManager = GetComponent<NetworkManager>();
+	}
+	
+    public void MyStartHost()
+    {
+        Debug.Log("Starting Host at: " + Time.timeSinceLevelLoad);
+        networkManager.StartHost();
+    }
+
+    void OnStartHost()
+    {
+        Debug.Log("Host started at: " + Time.timeSinceLevelLoad);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
