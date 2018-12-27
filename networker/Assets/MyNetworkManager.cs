40ba1ce09feb554dcfc055687aa02dedc5810aa4
Wojtek-PC\wojciech.stachowiak <stachowiakw@gmail.com>
2018-12-27 14:39:16 +0100

    3rd commit

---
+++ networker/Assets/MyNetworkManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+
+	// Use this for initialization
+	void Start () {
+        print("myNetworkManager has started");
+	}
+	
+    public void MyStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " starting Host.");
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " host started.");
+    }
+
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Client started.");
+        InvokeRepeating("printLoading", 0f, 0.1f);
+    }
+
+    public override void OnClientConnect(NetworkConnection connection)
+    {
+       Debug.Log(Time.timeSinceLevelLoad + " Client is connected to IP:" + connection.address);
+        CancelInvoke();
+    }
+
+    private void printLoading()
+    {
+        print(".");
+    }
+}
---
+++ networker/Assets/Players/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    private Vector3 inputValue;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        else
+        {
+            inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+            inputValue.y = 0f;
+            inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+            transform.Translate(inputValue);
+        }
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponentInChildren<Camera>().enabled = true;
+    }
+}
